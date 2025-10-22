01d7263b2e2da1604e5fdd1f11dcbb10bd31f5df
lord <cristoreymagdadaro20@gmail.com>
2025-10-22 12:38:29 +0800

    Add Facebook crawler detection and UA logging
    Updated .htaccess to add a condition for requests from the 'facebookexternalhit' user agent. Added error logging in header.php to log user agent and request URI for debugging Facebook crawler requests.

--- wp-content/themes/modern-gwt-wordpress/header.php
+++ wp-content/themes/modern-gwt-wordpress/header.php
@@ -211,7 +211,9 @@
         var template_directory = '<?php echo get_template_directory_uri() ?>';
     </script>
 </head>
-
+<?php
+error_log("FB_DEBUG UA: " . ($_SERVER['HTTP_USER_AGENT'] ?? '[none]') . " -- REQUEST: " . ($_SERVER['REQUEST_URI'] ?? '[none]'));
+?>
 <body <?php body_class(); ?>>
 
 <div id="accessibility-shortcuts">
