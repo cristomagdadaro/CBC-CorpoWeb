a09515206f719420699e2f7e6c83c4b8f04cecf9
lord <cristoreymagdadaro20@gmail.com>
2025-10-27 14:39:18 +0800

    Use form_option for siteurl and home fields
    Replaced home_url() with form_option() for the 'WordPress Address (URL)' and 'Site Address (URL)' input fields in options-general.php to ensure correct values are displayed and saved in the settings form.

--- wp-admin/options-general.php
+++ wp-admin/options-general.php
@@ -239,12 +239,12 @@ if ( ! is_multisite() ) {
 
 <tr>
 <th scope="row"><label for="siteurl"><?php _e( 'WordPress Address (URL)' ); ?></label></th>
-<td><input name="siteurl" type="url" id="siteurl" value="<?php echo home_url(); ?>"<?php disabled( defined( 'WP_SITEURL' ) ); ?> class="regular-text code<?php echo $wp_site_url_class; ?>" /></td>
+<td><input name="siteurl" type="url" id="siteurl" value="<?php form_option( 'siteurl' ); ?>"<?php disabled( defined( 'WP_SITEURL' ) ); ?> class="regular-text code<?php echo $wp_site_url_class; ?>" /></td>
 </tr>
 
 <tr>
 <th scope="row"><label for="home"><?php _e( 'Site Address (URL)' ); ?></label></th>
-<td><input name="home" type="url" id="home" aria-describedby="home-description" value="<?php echo home_url(); ?>"<?php disabled( defined( 'WP_HOME' ) ); ?> class="regular-text code<?php echo $wp_home_class; ?>" />
+<td><input name="home" type="url" id="home" aria-describedby="home-description" value="<?php form_option( 'home' ); ?>"<?php disabled( defined( 'WP_HOME' ) ); ?> class="regular-text code<?php echo $wp_home_class; ?>" />
 	<?php if ( ! defined( 'WP_HOME' ) ) : ?>
 <p class="description" id="home-description">
 		<?php
