3285f778ecd9b9ab70f2feba81ce88c3c7916e1d
lord <cristoreymagdadaro20@gmail.com>
2025-11-02 01:13:57 +0800

    Update theme styles and Tailwind config
    Regenerated Tailwind CSS and updated tailwind.config.cjs. Modified theme PHP and CSS files to adjust styles and possibly layout or markup, including changes to navigation and single post templates.

--- wp-content/themes/modern-gwt-wordpress/single.php
+++ wp-content/themes/modern-gwt-wordpress/single.php
@@ -19,7 +19,7 @@ include_once('inc/banner.php');
              <div class="large-12 container-main">
                 <header>
 	                <?php while ( have_posts() ) : the_post(); ?>
-                        <h1 class="entry-title sm:text-2xl text-lg font-bold text-gray-900 relative pl-6">
+                        <h1 class="entry-title sm:text-2xl text-lg font-bold text-[#000000] relative pl-6">
                             <span class="absolute left-0 top-0 bottom-0 w-2 bg-gradient-to-b from-[#1f5d2b] to-[#a2b917]"></span>
                             <?php the_title(); ?>
                         </h1>
